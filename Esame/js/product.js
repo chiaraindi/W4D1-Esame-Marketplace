534bbb4c5f75219f40b5fc341b4bbf25d6cd7beb
Chiara Indirli <chiaraindirli2@gmail.com>
2025-04-13 09:47:00 +0200

    test

--- Esame/js/product.js
+++ Esame/js/product.js
@@ -1,5 +1,5 @@
 const endpoint = 'https://striveschool-api.herokuapp.com/api/product/';
-const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2ZhMjY4ZWNjMjZmODAwMTU1Y2JlMzYiLCJpYXQiOjE3NDQ0NDcxMTgsImV4cCI6MTc0NTY1NjcxOH0.HdmxfRCksO4dE8rPwo7eBcBk6HBy9tqlm1JOs8BtsJQ'; // Sostituisci con il tuo token
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2ZhMjY4ZWNjMjZmODAwMTU1Y2JlMzYiLCJpYXQiOjE3NDQ0NDcxMTgsImV4cCI6MTc0NTY1NjcxOH0.HdmxfRCksO4dE8rPwo7eBcBk6HBy9tqlm1JOs8BtsJQ';
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
